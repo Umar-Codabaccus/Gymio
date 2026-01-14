d5e251bedcaf36dda5f90de7a321133f7520d45c
Your Name <you@example.com>
2026-01-14 17:25:58 +0400

    Update Owner Class

--- Gymio.Api/Domain/Entities/Owner.cs
+++ Gymio.Api/Domain/Entities/Owner.cs
@@ -4,7 +4,19 @@ namespace Gymio.Api.Domain.Entities
 {
     public sealed class Owner : Entity<Guid>
     {
+        private Owner(Guid userId)
+        {
+            UserId = userId;
+        }
         public Guid UserId { get; private set; }
         public User? User { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+
+        public static Owner? Create(Guid userId)
+        {
+            var owner = new Owner(userId);
+            return owner;
+        }
     }
 }
